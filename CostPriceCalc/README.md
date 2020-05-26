54dff397f63cf600680fc8d2b35a0c005cd32845
Layt-Lyffski <layt.mail@gmail.com>
2020-05-26 11:24:48 +0200

    reinit

---
+++ CostPriceCalc/README.md
@@ -0,0 +1,11 @@
+ <h1>Accountancy.prog<h1> 
+ <p>allow the user to input the tax percentage and to input all of brutto bills or invoices, and then output the tax value, brutto value and the netto value on screen<p>
+
+<h2>Usage Protocol:<h2>
+<ul>
+  <li>1. use only numbers (except the (y/n))</li>
+  <li>2. input your tax percentage</li>
+  <li>3. input all you brutto values</li>
+  <li>4. input a negative value to calculate</li>
+  <li>5. exit or restart program</li>
+<ul>
---
+++ CostPriceCalc/main.py
@@ -0,0 +1,34 @@
+
+
+vListPrice              = float(input("Entry your List Price: "))
+ptcPurchaseDiscount     = float(input("Entry your Purchase Discount (eg. 3 % = 0.03): "))
+ptcPurchaseCashDiscount = float(input("Entry your Purchase Cash Discount (eg. 4 % = 0.04): "))
+rvDeliveryCost          = float(input("Entry your Delivery Cost (if none insert 0): "))
+
+
+vPurchaseDiscount   = vListPrice * ptcPurchaseDiscount
+rvPurchaseDiscount  = round(vPurchaseDiscount, 2)
+
+rvTargetPurchasePrice = vListPrice - rvPurchaseDiscount
+
+vPurchaseCashDiscount = rvTargetPurchasePrice * ptcPurchaseCashDiscount
+rvPurchaseCashDiscount= round(vPurchaseCashDiscount, 2)
+
+vCashBuyingPrice = rvTargetPurchasePrice - rvPurchaseCashDiscount
+rvCashBuyingPrice = round(vCashBuyingPrice, 2)
+
+vCostPrice = rvCashBuyingPrice + rvDeliveryCost
+rvCostPrice = round(vCostPrice, 2)
+
+
+print(vListPrice, "List Price")
+print(rvPurchaseDiscount, "Purchase Discount")
+print(rvTargetPurchasePrice, "Taraget Purchase Price")
+print(rvPurchaseCashDiscount, "Purchase Cash Discout")
+print(rvCashBuyingPrice, "Cash buying Price")
+print(rvDeliveryCost, "Delivery Cost")
+print(rvCostPrice, "Cost Price")
+
+print(" ")
+
+x = input("Press any Key to exit")
